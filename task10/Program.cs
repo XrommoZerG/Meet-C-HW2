f0192dfc78503916144836341159a210283bb1bb
Artem <FxD1ag767@outlook.com>
2023-06-15 20:42:39 +0300

    Задание 10 выполнено

---
+++ task10/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+
+
+// Задание 10
+
+
+
+Console.WriteLine("Введите трёхзначну цифру");
+
+string? strnumber = Console.ReadLine();
+
+int number = Convert.ToInt32(strnumber);
+
+int otv = (number / 10) % 10;
+
+Console.WriteLine(otv);
+
